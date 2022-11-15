10ff77cae55194840314e790bfc5066e8aa7453a
jeslove <jerryjeslove@gmail.com>
2022-11-15 16:43:56 +0000

    modified

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 
 ob_start();
-header('Access-Control-Allow-Origin: http://localhost:4200');
+header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
 header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Max-Age: 3600");
